c75afc390cdc49a54ab069e13956a7e812c365dd
Nguyendddddddddd <np499638@gmail.com>
2023-11-20 18:41:41 +0700

    test git giao dien

--- QLKTX_AGU/QLKTX_AGU/Form1.Designer.cs
+++ QLKTX_AGU/QLKTX_AGU/Form1.Designer.cs
@@ -29,32 +29,55 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
             SuspendLayout();
             // 
             // groupBox1
             // 
-            groupBox1.Location = new Point(276, 185);
+            groupBox1.Location = new Point(201, 148);
+            groupBox1.Margin = new Padding(2, 2, 2, 2);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(300, 150);
+            groupBox1.Padding = new Padding(2, 2, 2, 2);
+            groupBox1.Size = new Size(218, 120);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "groupBox1";
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(589, 292);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(393, 407);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 2;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1251, 1050);
+            ClientSize = new Size(910, 840);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(groupBox1);
-            Margin = new Padding(3, 2, 3, 2);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private GroupBox groupBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }

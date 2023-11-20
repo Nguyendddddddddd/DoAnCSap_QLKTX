b55b876995d9cd48ebbcf0c2e5d77a126dd5f92a
HoaiNam1566 <hoainam1566@gmail.com>
2023-11-20 18:44:02 +0700

    thay doi form

--- QLKTX_AGU/QLKTX_AGU/Form1.Designer.cs
+++ QLKTX_AGU/QLKTX_AGU/Form1.Designer.cs
@@ -31,42 +31,55 @@
             groupBox1 = new GroupBox();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
+            button1 = new Button();
             SuspendLayout();
             // 
             // groupBox1
             // 
-            groupBox1.Location = new Point(201, 148);
-            groupBox1.Margin = new Padding(2, 2, 2, 2);
+            groupBox1.Location = new Point(276, 185);
+            groupBox1.Margin = new Padding(3, 2, 3, 2);
             groupBox1.Name = "groupBox1";
-            groupBox1.Padding = new Padding(2, 2, 2, 2);
-            groupBox1.Size = new Size(218, 120);
+            groupBox1.Padding = new Padding(3, 2, 3, 2);
+            groupBox1.Size = new Size(300, 150);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "groupBox1";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(589, 292);
+            textBox1.Location = new Point(610, 393);
+            textBox1.Margin = new Padding(4, 4, 4, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
+            textBox1.Size = new Size(170, 33);
             textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(393, 407);
+            textBox2.Location = new Point(540, 509);
+            textBox2.Margin = new Padding(4, 4, 4, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
+            textBox2.Size = new Size(170, 33);
             textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(346, 112);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(11F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(910, 840);
+            ClientSize = new Size(958, 585);
+            Controls.Add(button1);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
             Controls.Add(groupBox1);
-            Margin = new Padding(2, 2, 2, 2);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
@@ -79,5 +92,6 @@
         private GroupBox groupBox1;
         private TextBox textBox1;
         private TextBox textBox2;
+        private Button button1;
     }
 }

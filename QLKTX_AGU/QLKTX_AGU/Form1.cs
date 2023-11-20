25bdada99c6eff30dc09a28ad8b43a9ef27886d1
HoaiNam1566 <hoainam1566@gmail.com>
2023-11-20 18:24:36 +0700

    Dung Khung Du An

--- QLKTX_AGU/QLKTX_AGU/Form1.cs
+++ QLKTX_AGU/QLKTX_AGU/Form1.cs
@@ -1,10 +1,31 @@
-namespace QLKTX_AGU
+﻿namespace QLKTX_AGU
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "SINH VIÊN";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label3.Text = "PHÒNG";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label3.Text = "NHÂN VIÊN";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label3.Text = "ĐĂNG XUẤT";
         }
     }
 }

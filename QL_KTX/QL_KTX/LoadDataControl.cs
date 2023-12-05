2b622d7f1813e7f34f3627542bf2641c79a6044b
Nguyendddddddddd <np499638@gmail.com>
2023-12-05 08:55:08 +0700

    tao class load du lieu

---
+++ QL_KTX/QL_KTX/LoadDataControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_KTX
+{
+    internal class LoadDataControl
+    {
+        public void Load_DataComBoBox(ComboBox cbo, string SQL, string DisplayMember, string ValueMember)
+        {
+            ConnectData connectData = new ConnectData();
+            connectData.openConnect();
+            DataSet dataSet = connectData.ReadData(SQL);
+            cbo.DataSource = dataSet.Tables[0];
+            cbo.DisplayMember = DisplayMember;
+            cbo.ValueMember = ValueMember;
+            connectData.closeConnect();
+        }
+    }
+}
